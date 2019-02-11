067f63d83b125c5ccfa38d9aa9c7daab0ca8be69
sha-zha <adeline.servan@gmail.com>
2019-02-11 11:06:18 +0400

    ajout de liste et tache par modal, le modal disparait une fois l'ajout fait.

--- js/to_do_list.js
+++ js/to_do_list.js
@@ -1,50 +1,113 @@
-   var compteur = 1;
+$(document).ready(function (){
+  $(function () {
+    var compteur = 1; 
+              
+    $(".btn").button().click(function (event) {
+        event.preventDefault();
+    });
 
- //fonction pour ajouter des listes
-        function add_list(){
-          //création toutes les variables qu'on aura besoin 
-          var section = document.querySelector('main > section>.row');
+function add_tache() {
+                    
+    compteur++;
+
+    var selectedList = $('#list').find(":selected").text();
+    //on recherche les div ayant data-title pour insérer le nom de la var selectedList
+    var box = $('div').find("[data-title='" + selectedList + "']");
+    //on recherche ul de les div ayant comme attribut data-title
+    // on créer les éléments 
+    const ul = document.createElement('ul');
+    const li = document.createElement('li');
+    const trash = document.createElement('i');
+
+    //on récupère les valeurs de la tache et son description
+    var ajout_tache = $('#ajout_tache').val();
+    var description= $('#description').val();
+
+    //on ajoute les classes
+    $(ul).addClass('card-text');
+    $(li).addClass('list-group-item d-flex  justify-content-between align-items-center');
+    $(trash).addClass('fas fa-trash');
+    
+    //incrémentation du li
+    $(li).attr('id', +compteur);
+
+    //on créee la tache + sa descritpion à l'endroit où on a sélectionné 
+    $(li).append(document.createTextNode(ajout_tache + " - " + description));
+    box.append(ul);
+    $(ul).append(li);
+    $(li).append(trash);
+ 
+  
+}
+
+    
+   function add_list() {
+ //création toutes les variables qu'on aura besoin
+          compteur++;
+
           var div_main = document.createElement('div');
           var div_card = document.createElement('div');
           var div_body = document.createElement('div');
           var plus = document.createElement('i');
           var titre = document.createElement('h5');
-          var input = document.getElementById("add_input_list");
+          var div_btn = document.createElement('div');
           
-          // var data Stock la valeur H5
-          var data = input.value;
+          
+         
 
           //ajout des class aux éléments créés
-          div_main.classList.add('col-md-4', 'listes');
-          div_card.classList.add('card');
-          div_body.classList.add('card-body');
-          plus.classList.add('fas', 'fa-plus-circle');
-          titre.classList.add('card-title', 'text-center');
+          $(div_main).addClass('col-md-4 p-4');
+          $(div_card).addClass('card');
+          $(div_body).addClass('card-body');
+          $(plus).addClass('fas fa-plus-circle');
+          $(titre).addClass('card-title text-center');
+          $(div_btn).addClass('container text-center my-2');
           
-
-		
-		  //ajout l'attribut modal 
-          plus.setAttribute('data-toggle', 'modal');
-          plus.setAttribute('data-target', '#modal_tache');
+    
+          // on récupère le nom de la liste                 
+            var list_name = $('#listname').val();
+          
+          //ajout des attributes 
+          $(plus).attr('data-toggle', 'modal');
+          $(plus).attr('data-target', '#modal_tache');
+          $(div_body).attr('data-title', list_name);
 
           //ajout id aux card
-          div_main.id =this.compteur;
-
-          //créer le text contenu par la variable data 
-          titre.appendChild(document.createTextNode(data));
-
+          $(div_body).attr('id', + compteur);
           
+          //créer le text contenu par la variable data 
+          $(titre).append(document.createTextNode(list_name));
+
+          console.log(plus);
 
           //créations des éléments 
-          section.appendChild(div_main);
-          div_main.appendChild(div_card);
-          div_card.appendChild(div_body);
-          div_body.appendChild(plus);
-          div_body.appendChild(titre);	         
+          $('.row').append(div_main);
+          $(div_main).append(div_card);
+          $(div_main).append(div_btn);
+          $(div_card).append(div_body);
+          $(div_body).append(plus);
+          $(div_body).append(titre);
+                     
 
-        div_main.innerHTML += "<div class='container text-center my-2'><button class='btn' onclick='effacer("+compteur+")'>Supprimer</button></div>";
-			
-		this.compteur = this.compteur +1;
-          console.log(data);
-          console.log(div_main.id);
-        }
+        $(div_btn).html("<button class='btn btn-sup' onclick='effacer("+compteur+")'>Supprimer</button>");
+
+        //on crée l'option avec la valeur de la liste
+        $('#list').append('<option value="' + list_name + '" selected="selected">' + list_name + '</option>');
+              
+    }
+                
+     $("#btn-list").click(function (event) {
+        event.preventDefault();
+        add_list();
+        $('#modal_liste').modal('hide');
+                    
+      });
+                
+      $('#btn-tache').click(function (event){
+        event.preventDefault();
+        add_tache();
+        $('#modal_tache').modal('hide');
+      });
+             
+  });
+});
