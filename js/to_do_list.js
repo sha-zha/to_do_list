64084ee99b11f211c4e3a08b7b280f22d58e833b
sha-zha <adeline.servan@gmail.com>
2019-04-23 11:08:38 +0400

    ajout todo

--- js/to_do_list.js
+++ js/to_do_list.js
@@ -1,19 +1,94 @@
 $(document).ready(function (){
-  $(function () {
     var compteur = 1; 
               
     $(".btn").button().click(function (event) {
         event.preventDefault();
     });
 
-function add_tache() {
+     $("#btn-list").click(function (event) {
+        event.preventDefault();
+        add_list(compteur);
+        compteur++;
+        $('#modal_liste').modal('hide');
                     
-    compteur++;
+      });
+                
+      $('#btn-tache').click(function (event){
+        event.preventDefault();
+        add_tache(compteur);
+        //on incremente apres les operations
+        compteur ++; 
+        $('#modal_tache').modal('hide');
+      });
+
+      $('.fa-trash').click(function (event){
+        event.preventDefault();
+        effacer(compteur);
+      });  
+
+      $('.btn-sup').click(function (event){
+        event.preventDefault();
+        effacer(compteur);
+      });    
+});
+
+    function add_list(compteur) {
+          //création toutes les variables qu'on aura besoin
+          var div_main = document.createElement('div');
+          var div_card = document.createElement('div');
+          var div_body = document.createElement('div');
+          var plus = document.createElement('i');
+          var titre = document.createElement('h5');
+          var div_btn = document.createElement('div');
+
+          //ajout des class aux éléments créés
+          $(div_main).addClass('col-md-4 p-4');
+          $(div_card).addClass('card');
+          $(div_body).addClass('card-body');
+          $(plus).addClass('fas fa-plus-circle');
+          $(titre).addClass('card-title text-center');
+          $(div_btn).addClass('container text-center my-2');
+          
+    
+          // on récupère le nom de la liste                 
+            var list_name = $('#listname').val();
+            var idatr = $('.modal-liste').val();
+          //ajout des attributes 
+          $(plus).attr('data-toggle', 'modal');
+          $(plus).attr('data-target', '#modal_tache');
+          $(div_body).attr('data-title', list_name);
+          $(div_body).attr('data-id', idatr);
+
+          //ajout id aux card
+          $(div_main).attr('id', + compteur);
+          
+          //créer le text contenu par la variable data 
+          $(titre).append(document.createTextNode(list_name));
+
+          console.log(plus);
+
+          //créations des éléments 
+          $('.row').append(div_main);
+          $(div_main).append(div_card);
+          $(div_main).append(div_btn);
+          $(div_card).append(div_body);
+          $(div_body).append(plus);
+          $(div_body).append(titre);
+                     
+
+        $(div_main).append("<button class='btn btn-sup' onclick='effacer("+compteur+")'>Supprimer</button>");
+
+        //on crée l'option avec la valeur de la liste
+        $('#list').append('<option value="' + list_name + '" selected="selected">' + idatr+ '</option>');
+              
+    }
+    function add_tache(compteur) {
 
     var selectedList = $('#list').find(":selected").text();
     //on recherche les div ayant data-title pour insérer le nom de la var selectedList
     var box = $('div').find("[data-title='" + selectedList + "']");
     //on recherche ul de les div ayant comme attribut data-title
+
     // on créer les éléments 
     const ul = document.createElement('ul');
     const li = document.createElement('li');
@@ -35,79 +110,13 @@ function add_tache() {
     $(li).append(document.createTextNode(ajout_tache + " - " + description));
     box.append(ul);
     $(ul).append(li);
-    $(li).append(trash);
+    $('#'+compteur).append('<i class=" fas fa-trash" onclick="effacer('+compteur+')"></i>');
  
   
-}
+  }
 
-    
-   function add_list() {
- //création toutes les variables qu'on aura besoin
-          compteur++;
-
-          var div_main = document.createElement('div');
-          var div_card = document.createElement('div');
-          var div_body = document.createElement('div');
-          var plus = document.createElement('i');
-          var titre = document.createElement('h5');
-          var div_btn = document.createElement('div');
-          
-          
-         
-
-          //ajout des class aux éléments créés
-          $(div_main).addClass('col-md-4 p-4');
-          $(div_card).addClass('card');
-          $(div_body).addClass('card-body');
-          $(plus).addClass('fas fa-plus-circle');
-          $(titre).addClass('card-title text-center');
-          $(div_btn).addClass('container text-center my-2');
-          
-    
-          // on récupère le nom de la liste                 
-            var list_name = $('#listname').val();
-          
-          //ajout des attributes 
-          $(plus).attr('data-toggle', 'modal');
-          $(plus).attr('data-target', '#modal_tache');
-          $(div_body).attr('data-title', list_name);
-
-          //ajout id aux card
-          $(div_body).attr('id', + compteur);
-          
-          //créer le text contenu par la variable data 
-          $(titre).append(document.createTextNode(list_name));
-
-          console.log(plus);
-
-          //créations des éléments 
-          $('.row').append(div_main);
-          $(div_main).append(div_card);
-          $(div_main).append(div_btn);
-          $(div_card).append(div_body);
-          $(div_body).append(plus);
-          $(div_body).append(titre);
-                     
-
-        $(div_btn).html("<button class='btn btn-sup' onclick='effacer("+compteur+")'>Supprimer</button>");
-
-        //on crée l'option avec la valeur de la liste
-        $('#list').append('<option value="' + list_name + '" selected="selected">' + list_name + '</option>');
-              
-    }
-                
-     $("#btn-list").click(function (event) {
-        event.preventDefault();
-        add_list();
-        $('#modal_liste').modal('hide');
-                    
-      });
-                
-      $('#btn-tache').click(function (event){
-        event.preventDefault();
-        add_tache();
-        $('#modal_tache').modal('hide');
-      });
-             
-  });
-});
+   function effacer(compteur){
+    //console.log(compteur);
+    //syntaxe => $('#id')
+    $('#'+compteur).remove();
+    } 
